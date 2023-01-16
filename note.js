8d21726613ea2e7c1d9adf65960989d173801f5b
Katlego Komane <113278179+CynicsTech@users.noreply.github.com>
2023-01-16 04:36:26 +0200

    js

---
+++ note.js
@@ -0,0 +1,29 @@
+//declaring functions
+//function to get notes
+function obtainNote(){
+
+}
+//to save note, takes in an array of notes
+
+function savNotes(){
+
+}
+
+//function to create notes
+
+function createNote(id, noteContent){
+
+}
+
+
+//add notes 
+
+function addNote(){
+
+}
+
+//update notes
+
+function updateNote(id, newNoteContent){
+  
+}
