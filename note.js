6b5b24013adf9940ba646232d09aefb34fb9349b
Katlego Komane <113278179+CynicsTech@users.noreply.github.com>
2023-01-16 04:50:40 +0200

    ddd

--- note.js
+++ note.js
@@ -4,18 +4,27 @@ const buttonAdd = container.querySelector("btn");
 //declaring functions
 //function to get notes
 function obtainNote(){
-  
+    return JSON.parse(localStorage.getItem("sticky-note") || "[]");
 }
 //to save note, takes in an array of notes
 
 function savNotes(){
-
+  localStorage.setItem("sticky-note", JSON.stringify(notes));
 }
 
 //function to create notes
 
 function createNote(id, noteContent){
+  const elm = document.createElement("textarea");
 
+
+  elm.classList.add("note");
+  elm.value = content;
+  elm.placeholder = "Add a note";
+
+  
+
+  return elm;
 }
 
 
